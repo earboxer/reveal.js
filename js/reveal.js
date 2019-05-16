478e3c71096e70a8001b1ce44347f51f13f9032a
Zach DeCook <you@example.com>
2019-05-16 13:07:37 -0400

    * Dimensions: Allow mixing percent-based and absolute sizes

--- js/reveal.js
+++ js/reveal.js
@@ -2374,11 +2374,21 @@
 		// Slide width may be a percentage of available width
 		if( typeof size.width === 'string' && /%$/.test( size.width ) ) {
 			size.width = parseInt( size.width, 10 ) / 100 * size.presentationWidth;
+			// If width was a percentage, and height was a value,
+			if( typeof config.height === 'number' ) {
+				// scale the width by the width scale, so actual size is correct.
+				size.width = ( size.height / size.presentationHeight ) * size.width;
+			}
 		}
 
 		// Slide height may be a percentage of available height
 		if( typeof size.height === 'string' && /%$/.test( size.height ) ) {
 			size.height = parseInt( size.height, 10 ) / 100 * size.presentationHeight;
+			// If height was a percentage, and width was a value,
+			if( typeof config.width === 'number' ) {
+				// scale the height by the width scale, so actual size is correct.
+				size.height = ( size.width / size.presentationWidth ) * size.height;
+			}
 		}
 
 		return size;
